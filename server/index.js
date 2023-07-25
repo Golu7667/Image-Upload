4c6aecbb50ff40e947be1f263f219c04b56b9f28
unknown <goluchoudhary7808@gmail.com>
2023-07-25 16:04:40 +0530

    addport

--- server/index.js
+++ server/index.js
@@ -2,59 +2,9 @@ const express = require("express");
 const app = express();
 const router = require("./routes/router");
 const cors = require("cors");
-
-
-
-
-
-const multer = require("multer");
-
-
-// storage config
-const storage = multer.diskStorage({
-    destination:(req,file,callback)=>{
-        callback(null,"./uploads")
-    },
-    filename:(req,file,callback)=>{
-        const filename = `image-${Date.now()}.${file.originalname}`
-        callback(null,filename)
-    }
-});
-
-// filter 
-const filefilter = (req,file,callback)=>{
-    if(file.mimetype === "image/png" ||file.mimetype === "image/jpg" ||file.mimetype === "image/jpeg"  ){
-        callback(null,true)
-    }else{
-        callback(null,false)
-        return callback(new Error("Only .png .jpg & .jpeg formatted Allowed"))
-    }
-}
-
-const upload = multer({
-    storage:storage,
-    fileFilter:filefilter
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 require("dotenv").config();
 require("./db/conn");
-const port = 8000;
+
 
 
 app.use(express.json());
@@ -64,6 +14,6 @@ app.use(router);
 app.use("/uploads",express.static("./uploads"));
 
 
-app.listen(port,()=>{
+app.listen(process.env.PORT,()=>{
     console.log(`server start at port no ${port}`)
 })
